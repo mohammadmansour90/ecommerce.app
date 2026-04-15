d90e4500409a2032d2b23d5bfbfd1323620f0990
Mohammad Mansour <Mo7ammed.192@gmail.com>
2026-04-16 01:31:03 +0300

    Initial commit - ecommerce app

--- backend/server.js
+++ backend/server.js
@@ -8,23 +8,24 @@ import productRouter from './routes/productRoute.js'
 import cartRouter from './routes/cartRouter.js'
 import orderRouter from './routes/orderRoute.js'
 
-// App Config
 const app = express()
-const port = process.env.PORT || 4000
+
 connectDB()
 connectCloudinary()
-// middlewares
+
 app.use(express.json())
 app.use(cors())
 
-// api endpoints
-app.use('/api/user',userRouter)
-app.use('/api/product',productRouter)
-app.use('/api/cart',cartRouter)
-app.use('/api/order',orderRouter)
+// IMPORTANT:
+// Because Vercel mounts this backend at /api,
+// do NOT put /api again here.
+app.use('/user', userRouter)
+app.use('/product', productRouter)
+app.use('/cart', cartRouter)
+app.use('/order', orderRouter)
 
 app.get('/', (req, res) => {
   res.send('API Working')
 })
 
-app.listen(port, () => console.log('Server started on PORT : ' + port))
+export default app
